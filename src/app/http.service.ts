2ab08fe7f83484d82677230e651beb181ae84bc7
DanzigWebb <valverin95@gmail.com>
2020-02-25 01:02:56 +0300

    1 file modified

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class HttpService {
-  url = 'https://api.jsonbin.io/b/5df3c10a2c714135cda0bf0f/1'
+  url = 'https://api.jsonbin.io/b/5df3c10a2c714135cda0bf0f/1';
   constructor(private http: HttpClient) { }
   getData() {
     return this.http.get(this.url);
